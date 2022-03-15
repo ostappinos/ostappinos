6291b2bbec9404b90a7a6fac56f60310acf89ed6
ostappinos <101601517+ostappinos@users.noreply.github.com>
2022-03-15 05:10:18 +0100

    ostappino

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js")
+
+const client = new Discord.Client(
+
+    {intents: ["GUILDS", "GUILD_MEMBER","GUILD_MESSAGES"] }
+)
+
+client.login("OTUyODY5NzkxMjYwMDk0NDk0.Yi8TJQ.dOYH8r4cxyc7Yvxp6T7pgSO0qtE")
