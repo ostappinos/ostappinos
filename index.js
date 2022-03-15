e018f7e81bb49a93937beba1882eea5b48933620
ostappinos <101601517+ostappinos@users.noreply.github.com>
2022-03-15 05:36:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const Discord = require("discord.js")
-
 const client = new Discord.Client(
-
-    {intents: ["GUILDS", "GUILD_MEMBER","GUILD_MESSAGES"] }
+    { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
 )
 
 client.login(process.env.token)
+
+client.on("ready", () => {
+    console.log("Ostappinos ONLINE")
+})
+
+client.on("messageCreate", message => {
+    if (message.content == "!comando") {
+        message.channel.send("Ciao a tutti!")
+    }
+
+    if (message.content == "!prova") {
+        message.channel.send("sciao belo te puzza il pisello")
+    }
+})
